ba377ad8704d08642136f85858469881f6ea995f
22zcorr <22zcorr@athenian.org>
2021-02-09 19:16:32 +0000

    started the HW

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,10 +3,29 @@ import java.util.ArrayList;
 public class MyMain {
 
     // Sorts the ArrayList using insertion sort
-    public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
-        return null;
+    public static ArrayList<Integer> insertionSort(ArrayList<Integer> in) {
+        ArrayList<Integer> out = new ArrayList<>();
+        
+        return insertionSort(in,out,0);
     }
+    public static ArrayList<Integer> insertionSort(ArrayList<Integer> in, ArrayList<Integer> out, int spot) {
+        
+        int i =0;
+        while(in.size() > out.size() ){
+        int compare = in.get(spot);
+        if(i>out.size()-1){
+            out.add(compare);
+            spot++;
+        }else if(compare<=out.get(i)){
+            out.add(i,compare);
+            spot++;
+        }else{
+            i++;
+        }
+    }
+    return out;
+    }
+    
 
      // Merges two sorted arrays into one large combined
      // sorted array
